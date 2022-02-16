7cfceaebd0b8ec31b900830d34de5d0a9ef51e5e
Vanya A. Sergeev <v@sergeev.io>
2022-02-16 03:23:54 -0600

    test: implement unit tests for LPToken

--- test/LPToken.spec.ts
+++ test/LPToken.spec.ts
@@ -1,20 +1,16 @@
 import { expect } from "chai";
-import { ethers, network } from "hardhat";
+import { ethers } from "hardhat";
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
-import { TestERC20, TestERC721, TestLendingPlatform, TestNoteToken } from "../typechain";
-import { Vault } from "../typechain";
-import { IERC20Metadata } from "../typechain";
-
-import { extractEvent, expectEvent } from "./helpers/EventUtilities";
+import { TestERC20, Vault, LPToken } from "../typechain";
 
 describe("LPToken", function () {
   let accounts: SignerWithAddress[];
   let tok1: TestERC20;
   let vault: Vault;
-  let seniorLPToken: IERC20Metadata;
-  let juniorLPToken: IERC20Metadata;
+  let seniorLPToken: LPToken;
+  let juniorLPToken: LPToken;
 
   beforeEach("deploy fixture", async () => {
     accounts = await ethers.getSigners();
@@ -22,25 +18,59 @@ describe("LPToken", function () {
     const testERC20Factory = await ethers.getContractFactory("TestERC20");
     const vaultFactory = await ethers.getContractFactory("Vault");
 
-    tok1 = (await testERC20Factory.deploy("Token 1", "TOK1", ethers.utils.parseEther("1000"))) as TestERC20;
+    tok1 = (await testERC20Factory.deploy("WETH", "WETH", ethers.utils.parseEther("1000"))) as TestERC20;
     await tok1.deployed();
 
     vault = (await vaultFactory.deploy("Test Vault", "TEST", tok1.address, ethers.constants.AddressZero)) as Vault;
     await vault.deployed();
 
-    seniorLPToken = (await ethers.getContractAt("IERC20Metadata", await vault.lpToken(0))) as IERC20Metadata;
-    juniorLPToken = (await ethers.getContractAt("IERC20Metadata", await vault.lpToken(1))) as IERC20Metadata;
+    seniorLPToken = (await ethers.getContractAt("LPToken", await vault.lpToken(0))) as LPToken;
+    juniorLPToken = (await ethers.getContractAt("LPToken", await vault.lpToken(1))) as LPToken;
   });
 
   describe("#mint", async function () {
-    it("fails on invalid caller", async function () {});
+    it("fails on invalid caller", async function () {
+      await expect(seniorLPToken.mint(accounts[0].address, ethers.utils.parseEther("100"))).to.be.revertedWith(
+        "Caller is not the owner"
+      );
+
+      await expect(juniorLPToken.mint(accounts[0].address, ethers.utils.parseEther("100"))).to.be.revertedWith(
+        "Caller is not the owner"
+      );
+    });
   });
 
   describe("#redeem", async function () {
-    it("fails on invalid caller", async function () {});
+    it("fails on invalid caller", async function () {
+      await expect(
+        seniorLPToken.redeem(
+          accounts[0].address,
+          ethers.utils.parseEther("100"),
+          ethers.utils.parseEther("1000"),
+          ethers.constants.Zero
+        )
+      ).to.be.revertedWith("Caller is not the owner");
+
+      await expect(
+        juniorLPToken.redeem(
+          accounts[0].address,
+          ethers.utils.parseEther("100"),
+          ethers.utils.parseEther("1000"),
+          ethers.constants.Zero
+        )
+      ).to.be.revertedWith("Caller is not the owner");
+    });
   });
 
   describe("#withdraw", async function () {
-    it("fails on invalid caller", async function () {});
+    it("fails on invalid caller", async function () {
+      await expect(
+        seniorLPToken.withdraw(accounts[0].address, ethers.utils.parseEther("100"), ethers.utils.parseEther("1000"))
+      ).to.be.revertedWith("Caller is not the owner");
+
+      await expect(
+        juniorLPToken.withdraw(accounts[0].address, ethers.utils.parseEther("100"), ethers.utils.parseEther("1000"))
+      ).to.be.revertedWith("Caller is not the owner");
+    });
   });
 });
