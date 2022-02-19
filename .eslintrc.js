8c97350599ce43e04384a861f71e5713c1cc001b
Vanya A. Sergeev <v@sergeev.io>
2022-02-18 19:35:38 -0600

    eslintrc: disable object-shorthand rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    "object-shorthand": "off",
     "node/no-unsupported-features/es-syntax": [
       "error",
       { ignores: ["modules"] },
