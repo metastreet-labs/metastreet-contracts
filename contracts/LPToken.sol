3a92a286edc40b1ad7927679144f85ee34950c96
Vanya A. Sergeev <v@sergeev.io>
2022-02-25 15:54:53 -0600

    contracts: remove unused import in LPToken

--- contracts/LPToken.sol
+++ contracts/LPToken.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LPToken is ERC20 {
     /**************************************************************************/
