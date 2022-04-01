b5c5e64346b1f83061b3e810ed83f2ec4752278d
Vanya A. Sergeev <v@sergeev.io>
2022-04-01 03:40:15 -0500

    eslintrc: add unused args ignore pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,9 @@ module.exports = {
       { tryExtensions: [".ts", ".js", ".json"] },
     ],
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "_" },
+    ],
   },
 };
