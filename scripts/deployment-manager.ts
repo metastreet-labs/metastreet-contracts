c31032fa8ad740e5c4248020f76c138041d044ea
Vanya A. Sergeev <v@sergeev.io>
2022-05-04 19:44:41 -0500

    scripts: add vault name suffix to lp token names in deployment manager

--- scripts/deployment-manager.ts
+++ scripts/deployment-manager.ts
@@ -210,14 +210,14 @@ async function vaultDeploy(
   console.debug(`Loan Price Oracle: ${loanPriceOracle.address}`);
 
   const seniorLPToken = await upgrades.deployBeaconProxy(deployment.lpTokenBeacon, lpTokenFactory, [
-    "Senior LP Token",
+    "Senior LP " + name,
     seniorLPSymbol,
   ]);
   await seniorLPToken.deployed();
   console.debug(`Senior LP Token:   ${seniorLPToken.address}`);
 
   const juniorLPToken = await upgrades.deployBeaconProxy(deployment.lpTokenBeacon, lpTokenFactory, [
-    "Junior LP Token",
+    "Junior LP " + name,
     juniorLPSymbol,
   ]);
   await juniorLPToken.deployed();
