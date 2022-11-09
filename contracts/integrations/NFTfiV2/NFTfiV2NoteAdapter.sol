46f1d8364bfade664ccccbac817ed9276b6a71da
Vanya A. Sergeev <v@sergeev.io>
2022-11-08 20:00:11 -0600

    contracts/integrations: increment NFTfiV2NoteAdapter version to v1.2

--- contracts/integrations/NFTfiV2/NFTfiV2NoteAdapter.sol
+++ contracts/integrations/NFTfiV2/NFTfiV2NoteAdapter.sol
@@ -75,7 +75,7 @@ contract NFTfiV2NoteAdapter is INoteAdapter {
     /**
      * @notice Implementation version
      */
-    string public constant IMPLEMENTATION_VERSION = "1.1";
+    string public constant IMPLEMENTATION_VERSION = "1.2";
 
     /**
      * @notice Supported loan type
